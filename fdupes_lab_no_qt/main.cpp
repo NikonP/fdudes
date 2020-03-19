bac16079b0308396629d0954b5e7a75cddcee266
Nikon Podgorny <podgorny.nikon@yandex.ru>
2020-03-19 20:05:53 +0300

    Done noqt version

--- fdupes_lab_no_qt/main.cpp
+++ fdupes_lab_no_qt/main.cpp
@@ -36,53 +36,83 @@ WARNING: the code that follows will make you cry; a safety pig is provided below
 namespace fs = std::experimental::filesystem;
 using namespace std;
 
+// Пути к файлам по ключам типа unsigned long long
 typedef unordered_map<unsigned long long, list<fs::path>> ll_path_map;
 
-// Распределяет все файлы в указанной директории по из размеру
+// Распределяет все файлы в указанной директории по размеру
 ll_path_map sort_by_size(fs::path start_path);
 
+// Распределяет все файлы в указанной директории по хэшу
 ll_path_map sort_by_hash(list<fs::path> paths);
 
+// Сравнивает файлы по байтам
 bool compare_by_bytes(fs::path file_1, fs::path file_2);
 
 int main(int argc, char const *argv[]) {
+    // Проверяем ест ьли аргумент
     if(argc < 2) {
         cerr << "no directories specified" << '\n';
         return 1;
     }
 
+    // Получаем путь
     fs::path start_path = argv[1];
 
+    // Превращаем путь в абсолютный
     if(start_path.is_relative()) {
         start_path = fs::current_path() / argv[1];
     }
 
-    if(!fs::is_directory(start_path)) {
-        cerr << start_path << " is not a directory" << '\n';
-        return 1;
-    }
-
+    // Проверяем существование пути
     if(!fs::exists(start_path)) {
         cerr << "no such directory " << start_path << '\n';
         return 1;
     }
 
+    // Проверяем является ли путь директорией
+    if(!fs::is_directory(start_path)) {
+        cerr << start_path << " is not a directory" << '\n';
+        return 1;
+    }
+
+    // Получаем все файлы отсортированные по размеру
     ll_path_map files_by_size = sort_by_size(start_path);
 
-    for(auto [fsize, paths] : files_by_size) {
-        if(paths.size() > 1) {
-            ll_path_map files_by_hash = sort_by_hash(paths);
+    // Для каждой "корзины" сортируем по хэшам
+    for(auto [fsize, paths_by_size] : files_by_size) {
+        if(paths_by_size.size() > 1) {
+            // Получаем все файлы отсортированные по хэшам
+            ll_path_map files_by_hash = sort_by_hash(paths_by_size);
 
+            // В каждой "корзине" сравниваем файлы по байтам
             for(auto [hash, paths_by_hash] : files_by_hash) {
-                if(paths_by_hash.size() > 1) {
-                    for(auto it = paths_by_hash.begin(); it != --paths_by_hash.end(); it++) {
-                        auto second_it = it;
-                        second_it++;
-                        for(; second_it != paths_by_hash.end(); second_it++) {
-                            cout << *it << " | " << *second_it << " " << compare_by_bytes(*it, *second_it) << '\n';
+                // Если РАЗНЫЕ ФАЙЛЫ имеют ОДИН ХЭШ ничего не произойдет
+                // НО теоретически в одной "корзине" может быть несколько "корзин" дубликатов
+                // Поэтому проверем всё
+                while(paths_by_hash.size() > 1) {
+                    fs::path front = paths_by_hash.front();
+                    paths_by_hash.pop_front();
+
+                    list<fs::path> dups;
+                    dups.push_back(front);
+
+                    auto it = paths_by_hash.begin();
+                    while(it != paths_by_hash.end()) {
+                        if(compare_by_bytes(front, *it)) {
+                            dups.push_back(*it);
+                            it = paths_by_hash.erase(it);
                         }
                     }
-                    std::cout << "-----------------------------" << '\n';
+
+                    // Выводим найденные дубликаты
+                    unsigned long long dup_num = 0;
+                    if(dups.size() > 1) {
+                        cout << dups.size() << " duplicates found:\n";
+                        for(fs::path p : dups) {
+                            cout << "[" << dup_num++ << "] " << p << '\n';
+                        }
+                    }
+                    cout << '\n';
                 }
             }
         }
